c439dc7e60f5d7224f1b268d83c1146a778cc045
João Hernrique FQ <jo-henrique8@hotmail.com>
2025-02-01 09:34:43 -0300

    main structure

---
+++ src/main/java/com/cdci/example/cicd_pos/CicdPosApplication.java
@@ -0,0 +1,13 @@
+package com.cdci.example.cicd_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdci/example/cicd_pos/CicdPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdci.example.cicd_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
