50cb84d8eff099a1c5dd71d7bee2b1ffd301f041
Jhenrique44 <jo-henrique8@hotmail.com>
2025-02-01 10:23:16 -0300

    try work actions

--- src/main/java/com/cdci/example/cicd_pos/CicdPosApplication.java
+++ src/main/java/com/cdci/example/cicd_pos/CicdPosApplication.java
@@ -3,6 +3,9 @@ package com.cdci.example.cicd_pos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Main application class for the CI/CD POS application
+
 @SpringBootApplication
 public class CicdPosApplication {
 
